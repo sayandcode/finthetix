c29ebd0355207ccf1e0c1eb5ea73eb21ef204424
Sayand Sathish <sayandcode@gmail.com>
2024-01-13 15:49:04 +0530

    contracts/feat: Add unstaking function
    - Users can unstake their staked amount to get their tokens back.
    
    - Associated tests were added. They include tests for token contract
      balances, interactions with invalid amounts and from invalid
      addresses, and also to check staking contract internal balances for
      stakers.
    
    - The error for staking/unstaking from zero address was renamed to
      InvalidUserAddress
    
    - `view` visibilty modifier was added to `getCurrStakedBalance`.

--- contracts/src/FinthetixStakingContract.sol
+++ contracts/src/FinthetixStakingContract.sol
@@ -11,9 +11,19 @@ interface FSCErrors {
     error CannotStakeZeroAmount(address stakerAddr);
 
     /**
-     * @notice Staking cannot happen from zero address
+     * @notice Zero is not a valid amount of tokens to unstake.
      */
-    error StakerAddressCannotBeZeroAddress();
+    error CannotUnstakeZeroAmount(address stakerAddr);
+
+    /**
+     * @notice Zero address is not supported
+     */
+    error InvalidUserAddress();
+
+    /**
+     * @notice User should not be able to unstake more than they have staked
+     */
+    error CannotUnstakeMoreThanStakedAmount(address stakerAddr, uint256 amtToUnstake, uint256 availableStakedBal);
 }
 
 contract FinthetixStakingContract is FSCErrors {
@@ -23,7 +33,7 @@ contract FinthetixStakingContract is FSCErrors {
 
     function stake(uint256 amtToStake) external {
         // checks
-        if (msg.sender == address(0)) revert StakerAddressCannotBeZeroAddress();
+        if (msg.sender == address(0)) revert InvalidUserAddress();
         if (amtToStake == 0) revert CannotStakeZeroAmount(msg.sender);
 
         // effects
@@ -33,7 +43,23 @@ contract FinthetixStakingContract is FSCErrors {
         stakingToken.transferFrom(msg.sender, address(this), amtToStake);
     }
 
-    function getCurrStakedBalance() external returns (uint256) {
+    function unstake(uint256 amtToUnstake) external {
+        // checks
+        if (msg.sender == address(0)) revert InvalidUserAddress();
+        if (amtToUnstake == 0) revert CannotUnstakeZeroAmount(msg.sender);
+        uint256 balanceOfSender = amtStakedBy[msg.sender];
+        if (balanceOfSender < amtToUnstake) {
+            revert CannotUnstakeMoreThanStakedAmount(msg.sender, amtToUnstake, balanceOfSender);
+        }
+
+        // effects
+        amtStakedBy[msg.sender] -= amtToUnstake;
+
+        // interactions
+        stakingToken.transfer(msg.sender, amtToUnstake);
+    }
+
+    function getCurrStakedBalance() external view returns (uint256) {
         return amtStakedBy[msg.sender];
     }
 }
--- contracts/test/FinthetixStakingContract.t.sol
+++ contracts/test/FinthetixStakingContract.t.sol
@@ -27,9 +27,11 @@ contract FinthetixStakingContract_UnitTest is Test {
      * @param amtToStake The amount of tokens to stake with the contract
      * @param stakerAddr The address of the staker
      * @notice Tests whether the token balances are updated on the ERC20
-     *  side whenever there is a staking action
+     *  side whenever there is a staking/unstaking action
      */
-    function test_StakingTransfersTokensFromStakerToStakingContract(uint248 amtToStake, address stakerAddr) public {
+    function test_StakingTransfersTokensBetweenStakerAndStakingContract(uint248 amtToStake, address stakerAddr)
+        public
+    {
         // assumptions
         vm.assume(stakerAddr != address(0) && amtToStake != 0);
 
@@ -38,25 +40,37 @@ contract FinthetixStakingContract_UnitTest is Test {
 
         // setup
         deal(address(stakingToken), stakerAddr, amtToStake, true);
-        uint256 initTokenBalOfStakingContract = stakingToken.balanceOf(stakingContractAddr);
-        uint256 initTokenBalOfStaker = stakingToken.balanceOf(stakerAddr);
+        uint256 tokenBal1OfStakingContract = stakingToken.balanceOf(stakingContractAddr);
+        uint256 tokenBal1OfStaker = stakingToken.balanceOf(stakerAddr);
 
-        // act
+        // act 1 - stake
         _approveAndStake(stakerAddr, amtToStake);
 
-        // verify
-        uint256 finalTokenBalOfStakingContract = stakingToken.balanceOf(stakingContractAddr);
-        uint256 finalTokenBalOfStaker = stakingToken.balanceOf(stakerAddr);
-        uint256 expectedFinalTokenBalOfStakingContract = initTokenBalOfStakingContract + amtToStake;
-        uint256 expectedFinalTokenBalOfStaker = initTokenBalOfStaker - amtToStake;
+        // verify 1
+        uint256 tokenBal2OfStakingContract = stakingToken.balanceOf(stakingContractAddr);
+        uint256 tokenBal2OfStaker = stakingToken.balanceOf(stakerAddr);
+        uint256 expectedTokenBal2OfStakingContract = tokenBal1OfStakingContract + amtToStake;
+        uint256 expectedTokenBal2OfStaker = tokenBal1OfStaker - amtToStake;
         assertEq(
-            finalTokenBalOfStakingContract,
-            expectedFinalTokenBalOfStakingContract,
+            tokenBal2OfStakingContract,
+            expectedTokenBal2OfStakingContract,
             "Staking did not increase token balance of staking contract"
         );
+        assertEq(tokenBal2OfStaker, expectedTokenBal2OfStaker, "Staking did not decrease token balance of staker");
+
+        // act 2 - unstake
+        vm.prank(stakerAddr);
+        stakingContract.unstake(amtToStake);
+
+        // verify 2
+        uint256 tokenBal3OfStakingContract = stakingToken.balanceOf(stakingContractAddr);
+        uint256 tokenBal3OfStaker = stakingToken.balanceOf(stakerAddr);
         assertEq(
-            finalTokenBalOfStaker, expectedFinalTokenBalOfStaker, "Staking did not decrease token balance of staker"
+            tokenBal3OfStakingContract,
+            tokenBal1OfStakingContract,
+            "Unstaking did not decrease token balance of staking contract"
         );
+        assertEq(tokenBal3OfStaker, tokenBal1OfStaker, "Unstaking did not increase token balance of staker");
     }
 
     /**
@@ -77,6 +91,51 @@ contract FinthetixStakingContract_UnitTest is Test {
     }
 
     /**
+     * @notice Tests that users cannot stake 0 amount
+     */
+    function test_CanNotUnstakeInvalidAmt() public {
+        // setup
+        address stakerAddr = vm.addr(0xB0b);
+        uint64 initTokenBalForStaker = 10 ether;
+        deal(address(stakingToken), stakerAddr, initTokenBalForStaker, true);
+        _approveAndStake(stakerAddr, initTokenBalForStaker);
+
+        // act & verify
+        vm.prank(stakerAddr);
+        vm.expectRevert(abi.encodeWithSelector(FSCErrors.CannotUnstakeZeroAmount.selector, stakerAddr));
+        stakingContract.unstake(0);
+    }
+
+    /**
+     * @param stakerAddr The address trying to unstake
+     * @param _amtToStake The amount staked initially (unbounded)
+     * @param _amtToUnstake The amount attempted to unstake (unbounded)
+     * @notice Tests that error is thrown if trying to withdraw more than
+     *  staked amount
+     */
+    function test_CannotUnStakeMoreThanStakedAmount(address stakerAddr, uint248 _amtToStake, uint248 _amtToUnstake)
+        public
+    {
+        // assumptions
+        vm.assume(stakerAddr != address(0));
+
+        // setup
+        uint256 boundedAmtToStake = bound(_amtToStake, 1, type(uint256).max - 1);
+        uint256 boundedAmtToUnstake = bound(_amtToUnstake, boundedAmtToStake + 1, type(uint256).max); // unstake more than staked amt
+        deal(address(stakingToken), stakerAddr, boundedAmtToStake, true);
+        _approveAndStake(stakerAddr, boundedAmtToStake);
+
+        // act & verify
+        vm.prank(stakerAddr);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                FSCErrors.CannotUnstakeMoreThanStakedAmount.selector, stakerAddr, boundedAmtToUnstake, boundedAmtToStake
+            )
+        );
+        stakingContract.unstake(boundedAmtToUnstake);
+    }
+
+    /**
      * @notice Tests that zero address cannot stake
      */
     function test_CanNotStakeFromInvalidAddr() public {
@@ -87,32 +146,53 @@ contract FinthetixStakingContract_UnitTest is Test {
         vm.startPrank(stakerAddr);
 
         // act & verify
-        vm.expectRevert(FSCErrors.StakerAddressCannotBeZeroAddress.selector);
+        vm.expectRevert(FSCErrors.InvalidUserAddress.selector);
         stakingContract.stake(0);
         vm.stopPrank();
     }
 
     /**
+     * @notice Tests that zero address cannot unstake
+     */
+    function test_CanNotUnstakeFromInvalidAddr() public {
+        // setup
+        uint256 amtToUnstake = 10 ether;
+
+        // act & verify
+        vm.prank(address(0));
+        vm.expectRevert(FSCErrors.InvalidUserAddress.selector);
+        stakingContract.unstake(amtToUnstake);
+    }
+
+    /**
      * @param stakerAddr The address used to stake
-     * @param arrOfAmtsToStake An array of numbers which are used to alternatively stake and unstake
-     * @notice Tests whether the balances of the user are cumulatively updated when they stake. So we
-     *  use a randomly generated array of numbers to stake iteratively, and then make sure balances
+     * @param arrOfAmts An array of numbers which are used to alternatively stake and unstake
+     * @notice Tests whether the balances of the user are cumulatively updated when they stake/unstake. So we
+     *  use a randomly generated array of numbers to stake/unstake iteratively, and then make sure balances
      *  are accurate.
      */
-    function test_StakedBalancesOfStakerAreUpdated(address stakerAddr, uint248[] calldata arrOfAmtsToStake) public {
+    function test_StakedBalancesOfStakerAreUpdated(address stakerAddr, uint248[] calldata arrOfAmts) public {
         // assumptions
         vm.assume(stakerAddr != address(0));
 
         // setup
         uint256 totalAmtStaked = 0;
 
-        for (uint256 i = 0; i < arrOfAmtsToStake.length; i++) {
+        for (uint256 i = 0; i < arrOfAmts.length; i++) {
             // act
-            uint248 amtToStake = arrOfAmtsToStake[i];
-            if (amtToStake == 0) continue;
-            deal(address(stakingToken), stakerAddr, amtToStake, true);
-            _approveAndStake(stakerAddr, amtToStake);
-            totalAmtStaked += amtToStake;
+            uint248 thisAmt = arrOfAmts[i];
+            if (thisAmt == 0) continue;
+            if (thisAmt > totalAmtStaked) {
+                // stake the amount
+                deal(address(stakingToken), stakerAddr, thisAmt, true);
+                _approveAndStake(stakerAddr, thisAmt);
+                totalAmtStaked += thisAmt;
+            } else {
+                // unstake it
+                vm.prank(stakerAddr);
+                stakingContract.unstake(thisAmt);
+                totalAmtStaked -= thisAmt;
+            }
 
             // verify
             vm.prank(stakerAddr);
@@ -128,7 +208,7 @@ contract FinthetixStakingContract_UnitTest is Test {
      * @dev This is used to simplify the verbose task of approving and staking with the
      *  required contract
      */
-    function _approveAndStake(address stakerAddr, uint248 amtToApproveAndStake) private {
+    function _approveAndStake(address stakerAddr, uint256 amtToApproveAndStake) private {
         vm.startPrank(stakerAddr);
         stakingToken.approve(address(stakingContract), amtToApproveAndStake);
         stakingContract.stake(amtToApproveAndStake);
