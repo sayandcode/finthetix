4929bf7b1c9fb71c980e30eb6ad29261d460b412
Sayand Sathish <sayandcode@gmail.com>
2024-01-27 09:43:55 +0530

    web-app/feat: Add root route and customize theme
    The root route was added, and the theme was customized with css

---
+++ web-app/app/routes/_index/route.tsx
@@ -0,0 +1,21 @@
+import type { MetaFunction } from '@remix-run/node';
+import { Button } from '~/components/ui/button';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Finthetix' },
+    { name: 'description', content: 'Earn rewards by staking with Finthetix' },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <section className="flex flex-col gap-y-2 items-center justify-center h-screen text-center">
+        <h1 className="text-5xl sm:text-6xl font-bold">Finthetix</h1>
+        <p className="text-xl">Earn rewards by staking tokens</p>
+        <Button>Connect Wallet</Button>
+      </section>
+    </div>
+  );
+}
