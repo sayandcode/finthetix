8937931f63fa8d1d17b400c0833870dbd7e56e41
Sayand Sathish <sayandcode@gmail.com>
2024-02-11 17:52:21 +0530

    web-app/refactor: Rename `contract` private variable in FSC Handler
    - This prepares us to save other kinds of contract in the instance variables
    
    - Also renamed the private function `updateDecimals` with a leading
      underscore, for easy classification

--- web-app/app/contracts/FinthetixStakingContract.ts
+++ web-app/app/contracts/FinthetixStakingContract.ts
@@ -4,7 +4,7 @@ import { FinthetixRewardToken__factory, FinthetixStakingContract, FinthetixStaki
 import { getReadableERC20TokenCount } from '~/lib/utils';
 
 export default class FinthetixStakingContractHandler {
-  private contract: FinthetixStakingContract;
+  private stakingContract: FinthetixStakingContract;
 
   // these will definitely assigned in async constructor `make`
   private STAKING_TOKEN_DECIMALS: number = NaN;
@@ -12,12 +12,12 @@ export default class FinthetixStakingContractHandler {
 
   static async make(provider: BrowserProvider, dappInfo: DappInfo) {
     const handler = new this(provider, dappInfo);
-    await handler.updateDecimals(provider);
+    await handler._updateDecimals(provider);
     return handler;
   }
 
   private constructor(private provider: BrowserProvider, dappInfo: DappInfo) {
-    this.contract
+    this.stakingContract
       = FinthetixStakingContract__factory.connect(
         dappInfo.stakingContractAddr,
         provider,
@@ -25,7 +25,7 @@ export default class FinthetixStakingContractHandler {
   }
 
   async getUserData() {
-    const stakedAmt = await this.contract.viewMyStakedAmt();
+    const stakedAmt = await this.stakingContract.viewMyStakedAmt();
     const rewardAmt = await this._getRewardAmt();
     return {
       stakedAmt:
@@ -36,25 +36,27 @@ export default class FinthetixStakingContractHandler {
   }
 
   private async _getRewardAmt() {
-    const publishedReward = await this.contract.viewMyPublishedRewards();
+    const publishedReward = await this.stakingContract.viewMyPublishedRewards();
     const accruedReward = await this._calculateAccruedReward();
     const currRewardAmt = publishedReward + accruedReward;
     return currRewardAmt;
   }
 
   private async _calculateAccruedReward() {
-    const stakedAmt = await this.contract.viewMyStakedAmt();
-    const totalRewardsPerSec = await this.contract.TOTAL_REWARDS_PER_SECOND();
+    const stakedAmt = await this.stakingContract.viewMyStakedAmt();
+    const totalRewardsPerSec
+      = await this.stakingContract.TOTAL_REWARDS_PER_SECOND();
     const alphaNow
-      = await this.contract.alphaNow() + await this._calculateAccruedAlpha();
+      = await this.stakingContract.alphaNow()
+      + await this._calculateAccruedAlpha();
     const alphaOfUserAtLastInteraction
-      = await this.contract.viewAlphaAtMyLastInteraction();
+      = await this.stakingContract.viewAlphaAtMyLastInteraction();
     return stakedAmt * totalRewardsPerSec
       * (alphaNow - alphaOfUserAtLastInteraction);
   }
 
   private async _calculateAccruedAlpha() {
-    const totalStakedAmt = await this.contract.totalStakedAmt();
+    const totalStakedAmt = await this.stakingContract.totalStakedAmt();
     if (totalStakedAmt === 0n) return 0n;
 
     const currBlockNo = await this.provider.getBlockNumber();
@@ -64,22 +66,23 @@ export default class FinthetixStakingContractHandler {
       throw new Error(`Current block (${currBlockNo}) doesn't exist`);
 
     const blockTimestamp = BigInt(blockTimestampAsNumber);
-    const lastUpdatedRewardAt = await this.contract.lastUpdatedRewardAt();
-    const cooldownConstant = await this.contract.COOLDOWN_CONSTANT();
+    const lastUpdatedRewardAt
+      = await this.stakingContract.lastUpdatedRewardAt();
+    const cooldownConstant = await this.stakingContract.COOLDOWN_CONSTANT();
 
     return (blockTimestamp - lastUpdatedRewardAt) * cooldownConstant
       / totalStakedAmt;
   }
 
-  private async updateDecimals(provider: BrowserProvider) {
-    const stakingTokenAddr = await this.contract.stakingToken();
+  private async _updateDecimals(provider: BrowserProvider) {
+    const stakingTokenAddr = await this.stakingContract.stakingToken();
     this.STAKING_TOKEN_DECIMALS = Number(
       await FinthetixStakingToken__factory
         .connect(stakingTokenAddr, provider)
         .decimals(),
     );
 
-    const rewardTokenAddr = await this.contract.rewardToken();
+    const rewardTokenAddr = await this.stakingContract.rewardToken();
     this.REWARD_TOKEN_DECIMALS = Number(
       await FinthetixRewardToken__factory
         .connect(rewardTokenAddr, provider)
