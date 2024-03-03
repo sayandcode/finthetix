2022bd98654bc5b0c19ff806dc71c69c289b3539
Sayand Sathish <sayandcode@gmail.com>
2024-03-03 06:27:21 +0530

    web-app/refactor: Move homepage sections into their own components
    Forward looking change in anticipation of more sections being added to
    homepage

---
+++ web-app/app/routes/_index/subcomponents/HowItWorksSection/index.tsx
@@ -0,0 +1,32 @@
+export default function HowItWorksSection() {
+  return (
+    <section className="mx-8">
+      <h2 className="font-bold text-4xl mb-2">How it works</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.  Illo,
+        ratione sapiente ut id recusandae totam nesciunt culpa dicta odit
+        deleniti voluptas soluta vel ipsum commodi voluptatem distinctio
+        provident necessitatibus earum magnam, nostrum eius natus, beatae
+        perspiciatis. Suscipit fugit nam nisi quisquam voluptas blanditiis
+        eligendi fuga odit quibusdam minus aspernatur quod voluptate rem odio
+        est ipsa veniam aperiam vero explicabo, neque iure.  Debitis facilis
+        autem tempora sapiente aliquam! Cum reprehenderit velit odit magni
+        numquam, perferendis tempore. Odio ratione ex, error distinctio itaque
+        minima consectetur, nam cupiditate, a debitis recusandae eligendi
+        ducimus. Iste, repudiandae obcaecati dolor placeat sunt pariatur
+        asperiores, magni magnam, id perspiciatis consequuntur ut. Amet
+        perferendis accusamus repellat, vero dolore distinctio nemo saepe
+        molestias nam delectus fuga rerum maxime mollitia explicabo fugiat
+        maiores adipisci voluptatibus architecto earum corrupti hic officia
+        aspernatur doloremque? Eum dolores quod est expedita non suscipit quas
+        voluptate nostrum corporis, quos voluptatibus rem perferendis ullam
+        autem, assumenda quisquam eveniet eaque, quae minima voluptates iusto
+        praesentium quaerat! Iste ipsa officiis accusantium minus eius quam ex
+        hic dolorum harum tempora, nobis veritatis rem?  Culpa, provident
+        facere. Nisi porro molestias repellendus laudantium accusantium eaque
+        repudiandae ut fuga eligendi. Asperiores enim sit quidem! Culpa magnam
+        beatae possimus officiis voluptate saepe quasi.
+      </p>
+    </section>
+  );
+}
